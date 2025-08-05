01631f290b2569e29282b865f665186c88c56101
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-08-05 19:03:31 +0200

    Update test_edirect.py

--- tests/test_edirect.py
+++ tests/test_edirect.py
@@ -1,8 +1,9 @@
 """Test eDirect utilities."""
 
 import unittest
+from pathlib import Path
 
-from pubmed_downloader.edirect import search_pubmed
+from pubmed_downloader.edirect import get_edirect_directory, search_pubmed
 
 
 class TestEDirect(unittest.TestCase):
@@ -10,5 +11,8 @@ class TestEDirect(unittest.TestCase):
 
     def test_search_pubmed(self) -> None:
         """Test searching PubMed."""
+        d = get_edirect_directory()
+        self.assertIsInstance(d, Path)
+
         res = search_pubmed("bioregistry")  # should be minimum 17 results
         self.assertLessEqual(17, len(res))
